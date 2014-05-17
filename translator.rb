0ac7da311144585dbe27000d2daca0278c55b452
Andrei Cristian Petcu <andrei@ceata.org>
2014-05-17 14:03:05 +0300

    traverse_tree

--- translator.rb
+++ translator.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 require 'yaml'
+
+def traverse_tree(object)
+  object.each do |key, value|
+    if value.class == Hash
+      traverse_tree(value)
+    else
+      puts "#{key}-----#{value}\n"
+    end
+  end
+end
+
 translate_yaml = YAML.load_file('sk.yml')
-puts translate_yaml.to_yaml
+# puts translate_yaml.to_yaml
+traverse_tree(translate_yaml)
