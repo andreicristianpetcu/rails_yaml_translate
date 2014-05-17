21a424c9d0f0fff13a303c3a9f4de3c3c5ff6f2e
Andrei Cristian Petcu <andrei@ceata.org>
2014-05-17 16:09:31 +0300

    moved common stuff to translate_file

--- translator.rb
+++ translator.rb
@@ -6,28 +6,35 @@ require 'easy_translate'
 APP_CFG = YAML.load_file('config.yaml')
 EasyTranslate.api_key = APP_CFG['google_translate_key']
 
-def translate(text_to_translate)
-  # translated = EasyTranslate.translate(text_to_translate, :from => :sk, :to => :ro)
-  translated = text_to_translate + 'translated'
-  sleep(1.0/100.0)
+def translate(text_to_translate, lang)
+  # translated = EasyTranslate.translate(text_to_translate, :from => :sk, :to => lang)
+  translated = text_to_translate + 'translated' + lang.to_s
+  # sleep(1.0/100.0)
   translated
 end
 
-def traverse_tree(object)
+def traverse_tree(object, lang)
   object.each do |key, value|
     if value.class == Hash
-      value = traverse_tree(value)
+      value = traverse_tree(value, lang)
       object[key] = value
     else
       # puts "#{key}-----#{value}\n"
-      translated = translate(value)
+      translated = translate(value, lang)
       object[key] = translated
     end
   end
   object
 end
 
-translate_yaml = YAML.load_file('sk.yml')
-traverse_tree(translate_yaml)
-puts translate_yaml.to_yaml
-File.open('ro.yaml', 'w') {|f| f.write translate_yaml.to_yaml }
+def translate_file(file)
+  translate_yaml = YAML.load_file(file)
+  en = traverse_tree(translate_yaml, :en)
+  File.open('ro.yaml', 'w') {|f| f.write en.to_yaml }
+
+  translate_yaml = YAML.load_file(file)
+  ro = traverse_tree(translate_yaml, :ro)
+  File.open('en.yaml', 'w') {|f| f.write ro.to_yaml }
+end
+
+translate_file('sk.yml')
