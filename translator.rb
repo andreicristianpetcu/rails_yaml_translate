ed216ea880eb4cb082928c6c80416eba657ca8d3
Andrei Cristian Petcu <andrei@ceata.org>
2014-05-18 23:11:20 +0300

    better file recursion system

--- translator.rb
+++ translator.rb
@@ -7,9 +7,13 @@ APP_CFG = YAML.load_file('config.yaml')
 EasyTranslate.api_key = APP_CFG['google_translate_key']
 
 def translate(text_to_translate, lang)
-  # translated = EasyTranslate.translate(text_to_translate, :from => :sk, :to => lang)
-  translated = text_to_translate + ' translated ' + lang.to_s
-  # sleep(1.0/100.0)
+  if not text_to_translate.nil? then
+    # translated = EasyTranslate.translate(text_to_translate, :from => :sk, :to => lang)
+    puts "text_to_translate " + text_to_translate
+    puts "lang.to_s " + lang.to_s
+    translated = text_to_translate + ' translated ' + lang.to_s
+    # sleep(1.0/100.0)
+  end
   translated
 end
 
@@ -19,7 +23,7 @@ def traverse_tree(object, lang)
       value = traverse_tree(value, lang)
       object[key] = value
     else
-      # puts "#{key}-----#{value}\n"
+      puts "#{key}-----#{value}\n"
       translated = translate(value, lang)
       object[key] = translated
     end
@@ -27,22 +31,26 @@ def traverse_tree(object, lang)
   object
 end
 
-def translate_file(file, folder)
+def translate_file(file)
+  folder = File.dirname(file)
   translate_yaml = YAML.load_file(file)
   en = traverse_tree(translate_yaml, :en)
-  File.open(folder + 'en.yml', 'w') {|f| f.write en.to_yaml; puts f.path }
-  # puts en.to_yaml
+  File.open(folder + 'en.yml', 'w') {|f| f.write en.to_yaml}
+  # ; puts f.path }
+  # puts folder
+  puts "translating " + file
 
   translate_yaml = YAML.load_file(file)
   ro = traverse_tree(translate_yaml, :ro)
-  File.open(folder + 'ro.yml', 'w') {|f| f.write ro.to_yaml; puts f.path }
+  File.open(folder + 'ro.yml', 'w') {|f| f.write ro.to_yaml}
+  # ; puts f.path }
 end
 
 def find_files(base_path, flang)
   Dir.glob(base_path + "**/" + flang + ".yml") do |file|
     if !File.directory? file
       # puts file
-      translate_file(flang + ".yml", base_path)
+      translate_file(file)
     else
       find_files(file, flang)
     end
